31e6d12e42876f1a039490ce8cbcda36b67d9d8f
Bramigk <victor_bramigk@hotmail>
2014-05-20 08:10:55 -0300

    Storage now is possible!

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,11 @@
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 
 public class MainClass {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		AirportsGraph airports_graph = new AirportsGraph();
 		
 		// Construct graph
@@ -23,6 +25,22 @@ public class MainClass {
 		
 		System.out.println("\nTotal price: " + airports_graph.getBestPath().getWeight()+
 							"\nPath: "+airports_graph.getBestPath());
+		
+		// Save in memory
+		airports_graph.saveGraph("src/airport_graph.ser");
+		
+		// Load from memory
+        AirportsGraph g = new AirportsGraph();
+        g.loadGraph("src/airport_graph.ser");
+        
+        // get array of airports name
+        airports = g.getAirports();
+        
+		// Find shortest path from airports
+		g.findBestPath(airports.get(0),airports.get(airports.size()-1));
+        
+        System.out.println("\nTotal price: " + g.getBestPath().getWeight()+
+				"\nPath: "+g.getBestPath());
 	}
 
 }
