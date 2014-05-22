623aecf7101a2ebf87fa1105afc8e36c40da1197
Jonas Amaro <jonasrla@gmail.com>
2014-05-22 18:31:07 -0300

    change the value sent by request

--- WebServer/WebContent/WEB-INF/destino.js
+++ WebServer/WebContent/WEB-INF/destino.js
@@ -1,60 +1,60 @@
 var destinos = 
 [
-"--",
-"Aeroporto Internacional de Brasilia - Presidente Juscelino Kubitschek",
-"Aeroporto Internacional de Goiania - Santa Genoveva",
-"Aeroporto Internacional de Campo Grande",
-"Aeroporto Internacional Marechal Rondon",
-"Aeroporto Internacional de Rio Branco - Presidente Medici",
-"Aeroporto Internacional de Manaus - Eduardo Gomes",
-"Aeroporto Regional de Parintins",
-"Aeroporto Internacional de Macapa",
-"Aeroporto Internacional de Belem - Val de Cans",
-"Aeroporto de Maraba",
-"Aeroporto de Santarem - Maestro Wilson Fonseca",
-"Aeroporto Internacional de Porto Velho - Governador Jorge Teixeira de Oliveira",
-"Aeroporto Internacional de Boa Vista - Atlas Brasil",
-"Aeroporto de Palmas - Brigadeiro Lysias Rodrigues",
-"Aeroporto Internacional de Maceio - Zumbi dos Palmares",
-"Aeroporto de Ilheus - Jorge Amado",
-"Aeroporto de Porto Seguro",
-"Aeroporto Internacional de Salvador - Deputado Luis Eduardo Magalhaes",
-"Aeroporto Internacional de Fortaleza - Pinto Martins",
-"Aeroporto de Imperatriz - Guilherme Cortez",
-"Aeroporto Internacional de Sao Luis - Marechal Cunha Machado",
-"Aeroporto Internacional de Joao Pessoa - Presidente Castro Pinto",
-"Aeroporto de Petrolina",
-"Aeroporto Internacional do Recife - Gilberto Freyre",
-"Aeroporto de Teresina - Senador Petronio Portela",
-"Aeroporto Internacional de Natal - Augusto Severo",
-"Aeroporto Internacional de Aracaju - Santa Maria",
-"Aeroporto de Vitoria - Eurico de Aguiar Salles",
-"Aeroporto Internacional de Belo Horizonte-Confins",
-"Aeroporto de Uberlandia - Aeroporto Cesar Bombonato",
-"Aeroporto Santos Dumont",
-"Aeroporto Internacional do Rio de Janeiro-Galeao",
-"Aeroporto de Bauru-Arealva",
-"Aeroporto Internacional de Campinas - Viracopos",
-"Aeroporto de Presidente Prudente",
-"Aeroporto de Ribeirao Preto",
-"Aeroporto de Sao Jose do Rio Preto",
-"Aeroporto de Congonhas",
-"Aeroporto Internacional de Sao Paulo-Guarulhos",
-"Aeroporto Internacional de Curitiba - Afonso Pena",
-"Aeroporto Internacional de Foz do Iguacu - Cataratas",
-"Aeroporto de Londrina - Governador Jose Richa",
-"Aeroporto Internacional de Porto Alegre - Salgado Filho",
-"Aeroporto Internacional de Florianopolis - Hercilio Luz",
-"Aeroporto de Joinville - Lauro Carneiro de Loyola",
-"Aeroporto Internacional de Navegantes - Ministro Victor Konder",
+{Nome:"--", ICAO:"none"},
+{Nome:"Brasilia - Presidente Juscelino Kubitschek", ICAO:"BSB"},
+{Nome:"Goiania - Santa Genoveva", ICAO:"GYN"},
+{Nome:"Campo Grande", ICAO:"CGR"},
+{Nome:"Marechal Rondon", ICAO:"CGB"},
+{Nome:"Rio Branco - Presidente Medici", ICAO:"RBR"},
+{Nome:"Manaus - Eduardo Gomes", ICAO:"MAO"},
+{Nome:"Parintins", ICAO:""},																//
+{Nome:"Macapa", ICAO:"MCP"},
+{Nome:"Belem - Val de Cans", ICAO:"BEL"},
+{Nome:"Maraba", ICAO:"MAB"},
+{Nome:"Santarem - Maestro Wilson Fonseca", ICAO:"STM"},
+{Nome:"Porto Velho - Governador Jorge Teixeira de Oliveira", ICAO:"PVH"},
+{Nome:"Boa Vista - Atlas Brasil", ICAO:"BVB"},
+{Nome:"Palmas - Brigadeiro Lysias Rodrigues", ICAO:"PMW"},
+{Nome:"Maceio - Zumbi dos Palmares", ICAO:"MCZ"},
+{Nome:"Ilheus - Jorge Amado", ICAO:"IOS"},
+{Nome:"Porto Seguro", ICAO:"BPS"},
+{Nome:"Salvador - Deputado Luis Eduardo Magalhaes", ICAO:"SSA"},
+{Nome:"Fortaleza - Pinto Martins", ICAO:"FOR"},
+{Nome:"Imperatriz - Guilherme Cortez", ICAO:"IMP"},
+{Nome:"Sao Luis - Marechal Cunha Machado", ICAO:"SLZ"},
+{Nome:"Joao Pessoa - Presidente Castro Pinto", ICAO:"JPA"},
+{Nome:"Petrolina", ICAO:"PNZ"},
+{Nome:"Recife - Gilberto Freyre", ICAO:"REC"},
+{Nome:"Teresina - Senador Petronio Portela", ICAO:"THE"},
+{Nome:"Natal - Augusto Severo", ICAO:"NAT"},
+{Nome:"Aracaju - Santa Maria", ICAO:"RIA"},
+{Nome:"Vitoria - Eurico de Aguiar Salles", ICAO:"VIX"},
+{Nome:"Belo Horizonte-Confins", ICAO:"PLU"},
+{Nome:"Uberlandia - Cesar Bombonato", ICAO:"UDI"},
+{Nome:"Santos Dumont", ICAO:"SDU"},
+{Nome:"Rio de Janeiro-Galeao", ICAO:"GIG"},
+{Nome:"Bauru-Arealva", ICAO:""},																	//
+{Nome:"Campinas - Viracopos", ICAO:"VCP"},
+{Nome:"Presidente Prudente", ICAO:"PPB"},
+{Nome:"Ribeirao Preto", ICAO:"RAO"},
+{Nome:"Sao Jose do Rio Preto", ICAO:"SJP"},
+{Nome:"Congonhas", ICAO:"CGH"},
+{Nome:"Sao Paulo-Guarulhos", ICAO:"GRU"},
+{Nome:"Curitiba - Afonso Pena", ICAO:"BFH"},
+{Nome:"Foz do Iguacu - Cataratas", ICAO:"IGU"},
+{Nome:"Londrina - Governador Jose Richa", ICAO:"LDB"},
+{Nome:"Porto Alegre - Salgado Filho", ICAO:"POA"},
+{Nome:"Florianopolis - Hercilio Luz", ICAO:"FLN"},
+{Nome:"Joinville - Lauro Carneiro de Loyola", ICAO:"JOI"},
+{Nome:"Navegantes - Ministro Victor Konder", ICAO:"NVT"},
 ]
 
 function oriDes(){
 	for (var i=0; i<destinos.length; i++){
-		document.getElementById("origem").innerHTML += "<option value="+(i-1).toString()+">"+destinos[i]+"</option>";
+		document.getElementById("origem").innerHTML += "<option value="+destinos[i].ICAO+">"+destinos[i].Nome+"</option>";
 	}
 	for (var i=0; i<destinos.length; i++){
-		document.getElementById("destino").innerHTML += "<option value="+(i-1).toString()+">"+destinos[i]+"</option>";
+		document.getElementById("destino").innerHTML += "<option value="+destinos[i].ICAO+">"+destinos[i].Nome+"</option>";
 	}
 }
 
@@ -62,16 +62,17 @@ function excluiOpcao(otherSelect,properSelect){
 	var e1 = document.getElementById(properSelect);
 	var strUser1 = e1.options[e1.selectedIndex].value;
 	var e2 = document.getElementById(otherSelect);
-	var strUser2 = e2.options[e2.selectedIndex].value;	
-	if (!(strUser1 == -1 && strUser2 == -1)){
+	var strUser2 = e2.options[e2.selectedIndex].value;
+	alert(strUser1 +" "+ strUser2)
+	if (!(strUser1 == "none" && strUser2 == "none")){
 		document.getElementById(otherSelect).innerHTML = "";
 		for (var i=0; i<destinos.length; i++){
-			if(i-1!=strUser1){
-				if (i-1 == strUser2){
-					document.getElementById(otherSelect).innerHTML += "<option value="+(i-1).toString()+" selected>"+destinos[i]+"</option>";
+			if(destinos[i].ICAO!=strUser1){
+				if (destinos[i].ICAO == strUser2){
+					document.getElementById(otherSelect).innerHTML += "<option value="+destinos[i].ICAO+" selected>"+destinos[i].Nome+"</option>";
 				}
 				else{
-					document.getElementById(otherSelect).innerHTML += "<option value="+(i-1).toString()+">"+destinos[i]+"</option>";
+					document.getElementById(otherSelect).innerHTML += "<option value="+destinos[i].ICAO+">"+destinos[i].Nome+"</option>";
 				}
 			}
 		}
